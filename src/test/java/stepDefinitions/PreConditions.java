5119d2cb2a98749a075b26729aedfb28f8a71bd0
Mateo Florez <mateo.florez@jala-foundation.org>
2022-10-06 21:51:38 -0500

    basePage and loginPage implemente for POM

--- src/test/java/stepDefinitions/PreConditions.java
+++ src/test/java/stepDefinitions/PreConditions.java
@@ -1,13 +1,14 @@
 package stepDefinitions;
 import io.cucumber.java.en.Given;
 import org.openqa.selenium.WebDriver;
+import pages.LoginPage;
 
 public class PreConditions {
 
-    private WebDriver driver = Hooks.webDriver;
     @Given("the user goes to the page")
     public void the_user_goes_to_the_page(){
-        String value = driver.getTitle();
-        System.out.println(value);
+        LoginPage loginPage = new LoginPage(Hooks.webDriver);
+        loginPage.usernameInput.sendKeys("Admin");
+        System.out.println("si funcionó");
     }
 }
