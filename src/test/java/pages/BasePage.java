4ea1847e54653567c700fc7677feccead00b3c5c
Mateo Florez <mateo.florez@jala-foundation.org>
2022-10-07 14:51:25 -0500

    implementation of the wait element method

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,11 +1,21 @@
 package pages;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
-    public WebDriver driver;
-    public BasePage(WebDriver driver) {
-        this.driver = driver;
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
+    public BasePage(WebDriver driver, WebDriverWait wait) {
+        BasePage.driver = driver;
+        BasePage.wait = wait;
         PageFactory.initElements(driver, this);
+    }
+
+    protected void WaitUntilElementVisible(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
     }
 }
--- src/test/java/stepDefinitions/PreConditions.java
+++ src/test/java/stepDefinitions/PreConditions.java
@@ -1,14 +1,23 @@
 package stepDefinitions;
 import io.cucumber.java.en.Given;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.LoginPage;
+import java.time.Duration;
 
 public class PreConditions {
 
+    private WebDriver driver = Hooks.webDriver;
+    private WebDriverWait wait;
+    private int timeout = Integer.parseInt(System.getenv("timeout"));
+
+    public PreConditions() throws Exception {
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+    }
+
     @Given("the user goes to the page")
     public void the_user_goes_to_the_page(){
-        LoginPage loginPage = new LoginPage(Hooks.webDriver);
-        loginPage.usernameInput.sendKeys("Admin");
-        System.out.println("si funcionó");
+        LoginPage loginPage = new LoginPage(driver, wait);
+        loginPage.fillLoginCredentials();
     }
 }
