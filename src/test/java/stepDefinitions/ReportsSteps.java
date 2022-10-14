2449cdaa2450e43f44edfac491372c3f86547282
Mateo Florez <mateo.florez@jala-foundation.org>
2022-10-14 15:42:05 -0500

    implementation of negative test cases for the report page

--- src/test/java/stepDefinitions/ReportsSteps.java
+++ src/test/java/stepDefinitions/ReportsSteps.java
@@ -28,6 +28,7 @@ public class ReportsSteps {
     @When("the user creates a report with the name {string}")
     public void the_user_creates_a_report_with_the_name(String reportName) {
         ReportsPage reports = new ReportsPage(driver, wait);
+        name = reportName;
         reports.setReportName(reportName);
         reports.selectReportOption();
         reports.saveReport();
@@ -42,8 +43,7 @@ public class ReportsSteps {
     @When("the user filters the reports using the name {string}")
     public void the_user_filters_the_reports_using_the_name(String reportName) {
         ReportsPage reports = new ReportsPage(driver, wait);
-        name = reportName;
-        reports.filterReport(name);
+        reports.filterReport(reportName);
     }
 
     @Then("the report should appears in the list")
@@ -58,9 +58,47 @@ public class ReportsSteps {
         reports.deleteReport();
     }
 
-    @Then("the report is deleted")
-    public void the_report_is_deleted() {
+    @Then("the report should be deleted")
+    public void the_report_should_be_deleted() {
         ReportsPage reports = new ReportsPage(driver, wait);
-        reports.checkDeletedReport();
+        reports.checkMessage();
+    }
+
+    @When("the user tries to create another report with the same name")
+    public void the_user_tries_to_create_another_report_with_the_same_name() {
+        ReportsPage reports = new ReportsPage(driver, wait);
+        reports.setReportName(name);
+    }
+
+    @Then("the user should receive message the already exists")
+    public void the_user_should_receive_message_the_already_exists() {
+        ReportsPage reports = new ReportsPage(driver, wait);
+        reports.checkErrorNameMessage();
+    }
+
+    @When("the user tries to create a project without display field")
+    public void the_user_tries_to_create_a_project_without_display_field() {
+        ReportsPage reports = new ReportsPage(driver, wait);
+        reports.setReportName("test name");
+        reports.saveReport();
+    }
+
+    @Then("the user should receive a warning message and continue in add report page")
+    public void the_user_should_receive_a_warning_message_and_continue_in_add_report_page() {
+        ReportsPage reports = new ReportsPage(driver, wait);
+        reports.checkMessage();
+    }
+
+    @When("the user tries to create a report without name")
+    public void the_user_tries_to_create_a_report_without_name() {
+        ReportsPage reports = new ReportsPage(driver, wait);
+        reports.setReportName("");
+        reports.saveReport();
+    }
+
+    @Then("the user should receive required message")
+    public void the_user_should_receive_required_message() {
+        ReportsPage reports = new ReportsPage(driver, wait);
+        reports.checkErrorNameMessage();
     }
 }
